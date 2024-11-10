134600e0c3f7fdff7508ce0c2b848f37dc6fffcf
Drajad Kusuma Adi <drajadkusumaadi@gmail.com>
2024-11-10 21:34:19 +0700

    feat: Get files and simple dangerous chars analysis on filename

--- analysis/filename/filename.go
+++ analysis/filename/filename.go
@@ -5,59 +5,49 @@ package filenameanalysis
 import (
 	"fmt"
 	"strings"
-
-	"github.com/fatih/color"
 )
 
+// !Yep, I just use AI here, I'll do deeper research on more dangerous characters to add (or less non-dangerous characters to remove from the lists). But for now I'll settle with these lists
+// !Also add reasons, for example how %00 can be used to bypass file upload restriction, how control characters can harm the system, etc.
 // Generic strange chars
-var blacklist_generic = []string{
-	"/", "\\", ":", "?", "*", "\"", "'", "<", ">", "|", ",", "^", "~", "[", "]", "{", "}", ";", "(", ")", "\\0", "$", "\u200B", "\uFEFF",
-}
-// Control characters
-// !Untested
-var blacklist_control = []string{
+var blacklist = []string{
+	// Strange characters
+	"/", "\\", ":", "?", "*", "\"", "'", "<", ">", "|", ",", "^", "~", "[", "]", "{", "}", ";", "(", ")", "\\0", "$",
+
+	// Strange unicodes
+	"\u200B", // Zero-width space
+	"\uFEFF", // Zero-width no-break space
+
+	// URL-encodes
+	"%00", // Null byte
+	"%01", "%02", "%03", "%04", "%05", "%06", "%07", "%08", "%09", // Control characters
+	"%0A", "%0B", "%0C", "%0D", "%0E", "%0F", // Line feed and carriage return
+	"%20", // Space
+	"%21", "%23", "%24", "%26", "%27", "%28", "%29", "%2F", "%3A", "%3B", "%3C", "%3E", "%3F", "%40", "%5B", "%5D", "%60", // Special characters
+	"%7B", "%7D", "%7C", "%5E", "%5C", "%60", "%7A", "%7A", "%7B", "%7D", // Unicode characters
+	"%25", // Percent sign
+	"%22", "%23", "%24", "%26", "%27", "%28", "%29", "%2F", "%3A", "%3B", "%3C", "%3E", "%3F", "%40", "%5B", "%5D", "%60", "%7B", "%7D", "%7C", "%5E", "%5C", "%60", "%7A", "%7A", "%7B", "%7D", // HTML entities
+
+	// Control characters
 	"\x00","\x01","\x02","\x03","\x04","\x05","\x06","\x07","\x08","\x09","\x0A","\x0B","\x0C","\x0D","\x0E","\x0F","\x10","\x11","\x12","\x13","\x14","\x15","\x16","\x17","\x18","\x19","\x1A","\x1B","\x1C","\x1D","\x1E","\x1F",
-}
-// URL-encoded
-var blacklist_url = []string{
-	"%00", "%20", "%2E%2E%2F", "%22%20",
-}
-// Windows system files
-var blacklist_windows = []string{
+
+	// Windows system files
 	"CON","PRN","AUX","NUL","COM0","COM1","COM2","COM3","COM4","COM5","COM6","COM7","COM8","COM9","LPT0","LPT1","LPT2","LPT3","LPT4","LPT5","LPT6","LPT7","LPT8","LPT9",
 }
 
-// Check if the target is blacklisted
-// func blacklisted(arr []string, target string) bool {
-// 	for _, val := range arr {
-// 		if val == target {
-// 			return true
-// 		}
-// 	}
-// 	return false
-// }
-
-// Return chars in filename that matches:
-// "/", "\\", ":", "?", "*", "\"", "'", "<", ">", "|", ",", "^", "~", "[", "]", "{", "}", ";", "(", ")", "\\0", "$", "\u200B", "\uFEFF"
-func TestGeneric(filename string) () {
-	fmt.Println("Checking for strange characters in filename...");
-	counter := 0;
-	for _, char := range strings.Split(filename, "") {
-		bad := false;
-		for _, bl := range blacklist_generic {
-			if (bl == char) {
-				bad = true;
-			}
+// Detect and print suspicious chars
+func Check(filename string) () {
+	var counter []string = make([]string, 0)
+	for _, bl := range blacklist {
+		if (strings.Contains(filename, bl)) {
+			counter = append(counter, bl)
 		}
-
-		if bad {
-			color.Set(color.BgRed);
-			counter++;
-		}
-
-		fmt.Print(char)
-
-		color.Unset();
 	}
-	fmt.Printf("\nFound %v strange characters.", counter);
+	if (len(counter) > 0) {
+		fmt.Printf("\nFound %v suspicious characters on file %v:\n", len(counter), filename);
+		for _, c := range counter {
+			fmt.Printf("-> %v\n", c)
+		}
+		fmt.Printf("\n")
+	}
 }
--- main.go
+++ main.go
@@ -1,16 +1,109 @@
 package main
 
 import (
-	FA "literal-metaphor/filestigator/analysis/filename"
+	"flag"
 	"os"
+	"path/filepath"
+	"strings"
+
+	fa "filestigator/analysis/filename"
+
+	"github.com/fatih/color"
 )
 
 func main() {
-	var filenames []string = make([]string, 0);
+	// Declare flags
+	flag_dir := flag.String("dir", "", "Read all files in the specified directory")
+	flag_dir_recursive := flag.Bool("r", false, "Recursively read all files, must be used in pair with -dir")
+	flag_files := flag.String("files", "", "Accept multiple input of files to be read, separated by comma (,) without space")
 
-	filenames = append(filenames, os.Args[1:]...)
+	// Parse flags
+	flag.Parse()
 
+	// * Flag edge cases
+	// No essential flag specified
+	if *flag_dir == "" && *flag_files == "" {
+		color.Yellow("Exiting: No directory or file to be read specified")
+		return
+	}
+	// -dir with -files
+	if *flag_dir != "" && *flag_files != "" {
+		color.Yellow("Exiting: -dir and -files cannot be used at the same time")
+		return
+	}
+	// -r without -dir
+	if *flag_dir == "" && *flag_dir_recursive {
+		color.Yellow("Exiting: Recursive flag (-r) can only be used with -dir")
+		return
+	}
+	// ? are there more?
+
+	var filenames []string = make([]string, 0) // Keep track of filenames
+	var unreadables []string = make([]string, 0) // Keep track on file read errors
+
+	// Read on specified directory
+	// * Issues:
+	// 1. When getting files recursively, unreadable files are appended to unreadables, but with non-recursive option any unreadable will result in panic
+	if *flag_dir != "" {
+
+		if *flag_dir_recursive {
+			// Recursively get files
+			filepath.Walk(".", func(path string, info os.FileInfo, err error) error {
+				// Append unreadable files to unreadables
+				if err != nil {
+					unreadables = append(unreadables, path)
+					return nil
+				}
+
+				// Get the filename
+				if !info.IsDir() {
+					filenames = append(filenames, filepath.Base(path))
+				}
+
+				return nil
+			})
+
+		} else {
+			// Unrecursively get files
+			files, err := os.ReadDir(*flag_dir)
+			if err != nil {
+				panic(err)
+			}
+
+			// Append filename
+			for _, file := range files {
+				if (!file.IsDir()) {
+					filenames = append(filenames, file.Name())
+				}
+			}
+		}
+	}
+
+	// Read specified files
+	if *flag_files != "" {
+		// TODO: Find a way to allow space between values
+		filenamesList := strings.Split(*flag_files, ",")
+		for _, filename := range filenamesList {
+			// Check if filename exists
+			file, err := os.Open(filename)
+			if err != nil {
+				unreadables = append(unreadables, filename)
+			} else {
+				filenames = append(filenames, filename)
+			}
+
+			// Close the file
+			file.Close()
+		}
+	}
+
+	// Print all unreadables
+	for _, ur := range unreadables {
+		color.Red("Cannot read from " + ur)
+	}
+
+	// Process each files
 	for _, filename := range filenames {
-		FA.TestGeneric(filename);
+		fa.Check(filename)
 	}
 }
